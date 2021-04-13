8b611ac4ce657a08b9d392693ef7b21c7b7d51fd
Pablo Rosique Vidal <pableke@gmail.com>
2021-04-13 21:13:48 +0200

    Add fields merge to collections in myjson

--- src/dao/tests/myjson/menus.js
+++ src/dao/tests/myjson/menus.js
@@ -1,7 +1,17 @@
 
 module.exports = function(table) {
+	let _publicMenus;
+
+	table.onload = function(menus) {
+		_publicMenus = menus.filter(menu => menu.mask&1);
+	}
+
+	table.setField("href").setField("icon")
+		.setField("nombre").setField("orden")
+		.setField("mask").setField("f_creacion");
+
 	return {
 		getAll: table.getAll,
-		getPublic: function() { return table.filter(menu => menu.mask&1); }
+		getPublic: function() { return _publicMenus; }
 	}	
 }
