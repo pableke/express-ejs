568e4defb6765b928627214b702dc660478d0c74
pableke <pableke@gmail.com>
2021-04-13 08:55:06 +0200

    Add myjson persitence as dao

--- src/dao/tests/factory.js
+++ src/dao/tests/factory.js
@@ -1,10 +1,14 @@
 
+const myjson = require("../../lib/myjson.js");
+
 exports.open = function() {
+	myjson.open();
 	console.log("> DAO Tests Factory open.");
 	return this;
 };
 
 exports.close = function() {
+	myjson.close();
 	console.log("> DAO Tests Factory closed.");
 	return this;
 };
