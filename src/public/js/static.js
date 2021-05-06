6a5c9d551c067bc884880d614b6919c0313be05a
Pablo Rosique Vidal <pableke@gmail.com>
2021-05-06 17:09:36 +0200

    Resolve conflict with static files

--- src/public/js/static.js
+++ src/public/js/static.js
@@ -248,6 +248,147 @@ function JsBox() {
 }
 
 
+/**
+ * Message-Box module
+ * @module Message-Box
+ */
+function MessageBox() {
+	const self = this; //self instance
+	const EMPTY = ""; //empty string
+	const ZERO = "0";
+	const DOT = ".";
+	const COMMA = ",";
+
+	const sysdate = new Date(); //global sysdate
+	const RE_NO_DIGITS = /\D+/g; //split no digits
+	const daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; //january..december
+
+	const langs = {
+		en: { //english
+			lang: "en", //id iso
+			//inputs errors messages
+			errAjax: "Error on ajax call",
+			errForm: "Form validation failed",
+			errRequired: "Required field!",
+			errMinlength8: "The minimum required length is 8 characters",
+			errNif: "Wrong ID format",
+			errCorreo: "Wrong Mail format",
+			errDate: "Wrong date format",
+			errDateLe: "Date must be less or equals than current",
+			errDateGe: "Date must be greater or equals than current",
+			errNumber: "Wrong number format",
+			errGt0: "Price must be great than 0.00 &euro;", 
+			errRegex: "Wrong format",
+			errReclave: "Passwords typed do not match",
+			errRange: "Value out of allowed range",
+
+			//confirm cuestions
+			remove: "Are you sure to delete element?",
+			cancel: "Are you sure to cancel element?",
+
+			//inputs helpers functions
+			decimals: DOT, //decimal separator
+			intHelper: function(str, d) { return str && integer(str, COMMA); },
+			floatHelper: function(str, d) { return str && float(str, COMMA, DOT, 2); },
+			acDate: function(str) { return str && str.replace(/^(\d{4})(\d+)$/g, "$1-$2").replace(/^(\d{4}\-\d\d)(\d+)$/g, "$1-$2").replace(/[^\d\-]/g, EMPTY); },
+			acTime: function(str) { return str && str.replace(/(\d\d)(\d+)$/g, "$1:$2").replace(/[^\d\:]/g, EMPTY); },
+			dateHelper: function(str) { return str && fnDateHelper(splitDate(str)).join("-"); },
+			timeHelper: function(str) { return str && fnTimeHelper(str); }
+		},
+
+		es: { //spanish
+			lang: "es", //id iso
+			//inputs errors messages
+			errAjax: "Error en la llamada al servidor",
+			errForm: "Error al validar los campos del formulario",
+			errRequired: "Campo obligatorio!",
+			errMinlength8: "La longitud mínima requerida es de 8 caracteres",
+			errNif: "Formato de NIF / CIF incorrecto",
+			errCorreo: "Formato de E-Mail incorrecto",
+			errDate: "Formato de fecha incorrecto",
+			errDateLe: "La fecha debe ser menor o igual a la actual",
+			errDateGe: "La fecha debe ser mayor o igual a la actual",
+			errNumber: "Valor no numérico",
+			errGt0: "El importe debe ser mayor de 0,00 &euro;", 
+			errRegex: "Formato incorrecto",
+			errReclave: "Las claves introducidas no coinciden",
+			errRange: "Valor fuera del rango permitido",
+
+			//confirm cuestions
+			remove: "¿Confirma que desea eliminar este registro?",
+			cancel: "¿Confirma que desea cancelar este registro?",
+
+			//inputs helpers functions
+			decimals: COMMA, //decimal separator
+			intHelper: function(str, d) { return str && integer(str, DOT); },
+			floatHelper: function(str, d) { return str && float(str, DOT, COMMA, 2); },
+			acDate: function(str) { return str && str.replace(/^(\d\d)(\d+)$/g, "$1/$2").replace(/^(\d\d\/\d\d)(\d+)$/g, "$1/$2").replace(/[^\d\/]/g, EMPTY); },
+			acTime: function(str) { return str && str.replace(/(\d\d)(\d+)$/g, "$1:$2").replace(/[^\d\:]/g, EMPTY); },
+			dateHelper: function(str) { return str && swap(fnDateHelper(swap(splitDate(str)))).join("/"); },
+			timeHelper: function(str) { return str && fnTimeHelper(str); }
+		}
+	}
+
+	let _lang = langs.es; //default
+	function lpad(val) { return (+val < 10) ? (ZERO + val) : val; } //always 2 digits
+	function century() { return parseInt(sysdate.getFullYear() / 100); } //ej: 20
+	function splitDate(str) { return str.split(RE_NO_DIGITS).map(v => +v); } //int array
+	function swap(arr) { var aux = arr[2]; arr[2] = arr[0]; arr[0] = aux; return arr; }
+	function range(val, min, max) { return Math.min(Math.max(+val, min), max); } //force range
+	function rangeYear(yy) { return (yy < 100) ? (EMPTY + century() + lpad(yy)) : yy; } //autocomplete year=yyyy
+	function isLeapYear(year) { return ((year & 3) == 0) && (((year % 25) != 0) || ((year & 15) == 0)); } //año bisiesto?
+	function daysInMonth(y, m) { return daysInMonths[m] + ((m == 1) && isLeapYear(y)); }
+
+	function fnDateHelper(parts) {
+		parts[0] = rangeYear(parts[0]); //year
+		parts[1] = range(parts[1], 1, 12); //months
+		parts[2] = range(parts[2], 1, daysInMonth(parts[0], parts[1]-1)); //days
+		return parts.map(lpad);
+	}
+	function fnTimeHelper(str) {
+		let parts = splitDate(str);
+		parts[0] = range(parts[0], 0, 23); //hours
+		parts[1] = range(parts[1], 0, 59); //minutes
+		parts[2] = range(parts[2], 0, 59); //seconds
+		return parts.map(lpad).join(":");
+	}
+
+	function rtl(str, size) {
+		var result = []; //parts container
+		for (var i = str.length; i > size; i -= size)
+			result.unshift(str.substr(i - size, size));
+		(i > 0) && result.unshift(str.substr(0, i));
+		return result;
+	}
+	function integer(str, s) {
+		let sign = (str.charAt(0) == "-") ? "-" : EMPTY;
+		let whole = str.replace(RE_NO_DIGITS, EMPTY);
+		return isNaN(whole) ? str : (sign + rtl(whole, 3).join(s));
+	}
+	function float(str, s, d, n) {
+		let separator = str.lastIndexOf(_lang.decimals);
+		let sign = (str.charAt(0) == "-") ? "-" : EMPTY;
+		let whole = ((separator < 0) ? str : str.substr(0, separator))
+						.replace(RE_NO_DIGITS, EMPTY).replace(/^0+(\d+)/, "$1"); //extract whole part
+		let decimal = (separator < 0) ? ZERO : str.substr(separator + 1); //extract decimal part
+		let num = parseFloat(sign + whole + DOT + decimal); //float value
+		if (isNaN(num)) //is a valida number?
+			return str;
+		return sign + rtl(whole, 3).join(s) + d + ((separator < 0) ? ZERO.repeat(n) : decimal.padEnd(n, ZERO));
+	}
+
+	this.getLang = function(lang) { return lang ? langs[lang] : _lang; }
+	this.setLang = function(lang, data) { langs[lang] = data; return self; }
+	this.getI18n = function(lang) { return (lang) ? (langs[lang] || langs[lang.substr(0, 2)] || langs.es) : langs.es; }
+	this.setI18n = function(lang) { _lang = self.getI18n(lang); return self; }
+
+	this.get = function(name) { return _lang[name]; }
+	this.set = function(name, value) { _lang[name] = value; return self; }
+	this.sysdate = function() { return sysdate; }
+	this.format = function(str) {
+		return str.replace(/@(\w+);/g, (m, k) => { return nvl(_lang[k], m); });
+	}
+}
 
 
 //String Box extensions
@@ -704,12 +845,16 @@ valid.set("required", function(name, value, msgs) {
 	return valid.size(value, 1, 200) || !valid.setError(name, msgs.errRequired);
 }).set("min8", function(name, value, msgs) {
 	return valid.size(value, 8, 200) || !valid.setError(name, msgs.errMinlength8);
+}).set("max200", function(name, value, msgs) { //empty or length le than 200 (optional)
+	return valid.size(value, 0, 200) || !valid.setError(name, msgs.errMaxlength);
+}).set("token", function(name, value, msgs) {
+	return valid.size(value, 200, 800);
 }).set("usuario", function(name, value, msgs) {
 	return valid.min8(name, value, msgs) && (valid.idES(name, value) || valid.email(name, value) || !valid.setError(name, msgs.errRegex));
 }).set("clave", function(name, value, msgs) {
 	return valid.min8(name, value, msgs) && (valid.login(name, value) || !valid.setError(name, msgs.errRegex));
-}).set("reclave", function(name, value, msgs, data) {
-	return valid.clave(name, value, msgs) && ((value == data.clave) || !valid.setError(name, msgs.errReclave));
+}).set("reclave", function(name, value, msgs) {
+	return valid.clave(name, value, msgs) && ((value == valid.getData("clave")) || !valid.setError(name, msgs.errReclave));
 }).set("nif", function(name, value, msgs) {
 	return valid.required(name, value, msgs) && (valid.idES(name, value) || !valid.setError(name, msgs.errNif));
 }).set("correo", function(name, value, msgs) {
@@ -843,9 +988,6 @@ js.ready(function() {
 	/*************** validator-cli ***************/
 	/*********************************************/
 	// Extends validator-box for clients
-	function fnTocken(name, value, msgs) {
-		return valid.size(value, 200, 800);
-	}
 	valid.setForm("/login.html", {
 		usuario: valid.usuario,
 		clave: valid.clave
@@ -855,13 +997,23 @@ js.ready(function() {
 		asunto: valid.required,
 		info: valid.required
 	}).setForm("/signup.html", {
-		token: fnTocken,
+		token: valid.token,
 		nombre: valid.required,
 		ap1: valid.required,
 		nif: valid.nif,
 		correo: valid.correo
 	}).setForm("/reactive.html", {
-		token: fnTocken,
+		token: valid.token,
+		correo: valid.correo
+	}).setForm("/user/pass.html", {
+		oldPass: valid.min8,
+		clave: valid.min8,
+		reclave: valid.reclave
+	}).setForm("/user/profile.html", {
+		nombre: valid.required,
+		ap1: valid.required,
+		ap2: valid.max200, //optional
+		nif: valid.nif,
 		correo: valid.correo
 	});
 
