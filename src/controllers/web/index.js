7868f323ca7ee1b6edbc2cdac49266ec8aaed9c4
Pablo Rosique Vidal <pableke@gmail.com>
2021-05-11 12:31:03 +0200

    Reorganize routes for handling errors

--- src/controllers/web/index.js
+++ src/controllers/web/index.js
@@ -1,8 +1,4 @@
 
-const login = require("./public/login.js");
-
 exports.index = function(req, res) {
 	res.build("web/index");
 }
-
-exports.auth = login.auth;
--- src/routes/web/routes.js
+++ src/routes/web/routes.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const web = require("app/controllers/web/index.js");
+const login = require("app/controllers/web/public/login.js");
 
 router.use(require("./public.js"));
 router.get("/", web.index).get("/index.html", web.index).get("/home.html", web.index).get("/inicio.html", web.index);
-router.use("/menu", web.auth, require("./menu.js"));
-router.use("/user", web.auth, require("./user.js"));
+router.use("/menu", login.auth, require("./menu.js"));
+router.use("/user", login.auth, require("./user.js"));
 
 module.exports = router;
