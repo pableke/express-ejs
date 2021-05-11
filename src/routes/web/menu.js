d847abe7046d025facb6913a7c365fcef2a06e8f
Pablo Rosique Vidal <pableke@gmail.com>
2021-05-11 12:36:01 +0200

    Reorganize routes for handling errors

--- src/routes/web/menu.js
+++ src/routes/web/menu.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const ctrl = require("app/controllers/index.js");
 const admin = require("app/controllers/web/menu/admin.js");
 
+router.get("/", admin.list).get("/index.html", admin.list).get("/home.html", admin.list).get("/inicio.html", admin.list);
 router.get("/list", admin.list).get("/view.html", admin.view).get("/delete.html", admin.delete);
 router.post("/save.html", ctrl.post, admin.save, admin.error);
 
