a31801839c847702e4949cd2c117288ea5ecc359
Pablo Rosique Vidal <pableke@gmail.com>
2021-05-18 10:24:50 +0200

    Add css to pagination

--- src/public/js/tests/form.js
+++ src/public/js/tests/form.js
@@ -2,20 +2,7 @@
 js.ready(function() {
 	const msgs = i18n.getLang(); //messages container
 
-	// Tests form validators
-	valid.setForm("/tests/email.html", {
-		nombre: valid.required,
-		correo: valid.correo,
-		date: function(name, value, msgs) { //optional input
-			return !value || valid.ltNow(name, value, msgs);
-		},
-		number: valid.gt0,
-		asunto: valid.required,
-		info: function(name, value, msgs) {
-			return valid.size(value, 1, 600) || !valid.setError(name, msgs.errRequired);
-		}
-	});
-
+	// Range datepickers
 	const f1 = $("#f1").on("change", function() { f2.datepicker("option", "minDate", msgs.toDate(this.value)); });
 	const f2 = $("#f2").on("change", function() { f1.datepicker("option", "maxDate", msgs.toDate(this.value)); });
 });
---
+++ src/public/js/tests/main.js
@@ -0,0 +1,14 @@
+
+// Test module validators
+valid.setForm("/tests/email.html", {
+	nombre: valid.required,
+	correo: valid.correo,
+	date: function(name, value, msgs) { //optional input
+		return !value || valid.ltNow(name, value, msgs);
+	},
+	number: valid.gt0,
+	asunto: valid.required,
+	info: function(name, value, msgs) {
+		return valid.size(value, 1, 600) || !valid.setError(name, msgs.errRequired);
+	}
+});
