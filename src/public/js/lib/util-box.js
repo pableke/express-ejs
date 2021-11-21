b95c29990937cc3ce07bd94e9755e9269fcd7443
Pablo Rosique Vidal <pablo.rosique@upct.es>
2021-11-21 20:07:25 +0100

    Improve client config in util-box module

--- src/public/js/lib/util-box.js
+++ src/public/js/lib/util-box.js
@@ -1,11 +1,12 @@
 
+// Configure JS client
 const ab = new ArrayBox();
 const dt = new DateBox();
 const gb = new GraphBox();
 const nb = new NumberBox();
-const ob = new ObjectBox();
 const sb = new StringBox();
 const tb = new TreeBox();
 
 const i18n = new I18nBox();
 const js = new DomBox();
+i18n.setI18n(js.getLang());
