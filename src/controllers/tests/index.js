2a36ac668c2b2da965c779a9da110cf1edf8fb79
pableke <pableke@gmail.com>
2022-03-08 20:49:42 +0100

    Update xls function in util-box

--- src/controllers/tests/index.js
+++ src/controllers/tests/index.js
@@ -20,11 +20,19 @@ exports.email = (req, res, next) => {
 }
 
 exports.xls = function(req, res) {
-	util.xls({
+	util.xls(res, {
+		filename: "tests.xlsx",
 		author: "Pablo Rosique Vidal",
 		headers: { name: "Nombre", imp: "Importe", fCreacion: "Fecha" },
-		styles: { imp: { numberFormat: "#.##0,00" }, fCreacion: { dateFormat: "dd/mm/yyyy" } },
-		data: [{name: "askjld", imp: 23.87, fCreacion: new Date()}, {name: "fila2"}]
+		styles: {
+			imp: { numberFormat: "#,##0.00" },
+			fCreacion: { dateFormat: "dd/mm/yyyy" }
+		},
+		data: [
+			{ name: "askjld", imp: 23.87, fCreacion: new Date() },
+			{ name: "fila 2", imp:2.9 },
+			{ name: "fila 3", imp:24566.95675, fCreacion: "2022-01-01T10:22:53" }
+		]
 	});
 }
 
