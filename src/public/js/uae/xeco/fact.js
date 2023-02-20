6cf727643b00d0f1b973cdfb972ac5d60ac4e4ca
Pablo Rosique Vidal <pablo.rosique@upct.es>
2023-02-20 16:50:21 +0100

    Add fact module to uae project

--- src/public/js/uae/xeco/fact.js
+++ src/public/js/uae/xeco/fact.js
@@ -14,13 +14,17 @@ dom.ready(function() {
 		factura: {
 			msgError: "errForm", 
 			tercero: i18n.required, delegacion: i18n.required, 
-			organica: i18n.required, economica: i18n.digits,
-			memoria: i18n.required
+			organica: i18n.required, memoria: i18n.required
 		},
-		lineas: { msgError: "errForm", linea: i18n.required, imp: i18n.gt0 }
+		lineas: { msgError: "errForm", desc: i18n.required, imp: i18n.gt0 }
+	};
+	const ECONOMICAS = {
+		1: "132500", 2: "155100", 3: "131200", 4: "131000", 5: "131600", 6: "154000",
+		7: "132600", 8: "13250200", 9: "131300", 10: "131501", 12: "174100" //13: "174109"
 	};
 
 	const lineas = ab.parse(dom.getText("#lineas-json")) || [];
+	const updateEconomica = eco => dom.setValue("#economica", ECONOMICAS[eco]);
 	const fnCalcIva = (row, value) => {
 		RESUME.iva = +value; // nuevo iva
 		const iva = RESUME.imp * (RESUME.iva / 100);
@@ -28,30 +32,33 @@ dom.ready(function() {
 			.setText(row.nextElementSibling.cells[1], i18n.isoFloat(RESUME.imp + iva) + " €");
 	}
 
+	RESUME.iva = +dom.getValue("#iva") ?? 0;
+	VALIDATORS.factura.economica = FACT.uae ? i18n.required : null;
+
 	dom.click("a#add-linea", el => {
-		const data = dom.validate("#xeco-fact", VALIDATORS.lineas);
-		if (data) {
-			lineas.push(data); // save container
+		if (dom.validate("#xeco-fact", VALIDATORS.lineas)) {
+			lineas.push(i18n.toData()); // save container
 			dom.table("#conceptos", lineas, RESUME, STYLES);
 		}
 		dom.setValue("#linea", "").setValue("#imp", "").setFocus("#linea")
 	});
-	dom.table("#conceptos", lineas, RESUME, STYLES)
-		.onChangeTable("#conceptos", table => fnCalcIva(RESUME.row, RESUME.element.value));
 	dom.onRenderTable("#conceptos", table => {
 		RESUME.imp = 0; // Init. resume
 		lineas.forEach(linea => { RESUME.imp += linea.imp; }); // recalcula total
 		dom.setValue("#lineas", JSON.stringify(lineas)); // save data to send to server
 	});
-	dom.afterRenderTable("#conceptos", table => {
-		const iva = table.tFoot.querySelector("#iva");
-		if (iva) {
-			iva.value = RESUME.iva;
+	if (FACT.uae && (FACT.tipo == 1)) {
+		dom.afterRenderTable("#conceptos", table => {
+			table.tFoot.querySelector("#iva").value = RESUME.iva;
 			fnCalcIva(table.tFoot.rows[1], RESUME.iva);
-		}
-	});
+		});
+	}
+	dom.table("#conceptos", lineas, RESUME, STYLES)
+		.onChangeTable("#conceptos", table => fnCalcIva(RESUME.row, RESUME.element.value));
 
 	dom.swapAttr("#fMax", "min", "max")
+		.onChangeInputs("#subtipo", el => updateEconomica(el.value))
+		.onChangeInputs(".face-common", el => dom.eachInput(".face-common", input => { input.value = el.value; }))
 		.onChangeSelect("#delegacion", el => dom.setValue("#id-delegacion", el.value))
 		.onChangeSelect("#sujeto", el => dom.toggleHide(".grupo-exento", (el.value != 0)));
 	dom.onChangeSelect("#face", el => {
@@ -63,19 +70,22 @@ dom.ready(function() {
 			.toggleHide(".grupo-deportes", (el.value != 10));
 	});
 
+	const fnInsert = form => updateEconomica(dom.getValue("#subtipo"));
+	dom.onLoadForm("#xeco-fact", fnInsert, () => {});
+
 	window.fnSend = () => {
-		const data = dom.validate("#xeco-fact", VALIDATORS.factura);
-		if (data) {
-			ab.size(lineas) || dom.required("#linea", "Debe detallar los conceptos asociados a la solicitud.");
-			if (((data.subtipo == 3) || (data.subtipo == 9)) && !data.refreb) //subtipo = ttpp o extension
-				dom.addError("#recibo", "Debe indicar un número de recibo válido");
-			if (data.subtipo == 10) //subtipo = deportes
-				dom.required("#extra", VALIDATORS.msgError).leToday("#fMax", VALIDATORS.msgError);
-			if ((data.face == 1)) //factura electronica FACe
-				dom.required("#og", VALIDATORS.msgError).required("#oc", VALIDATORS.msgError).required("#ut", VALIDATORS.msgError);
-			if ((data.face == 2)) //factura electronica Otras
-				dom.required("#plataforma", VALIDATORS.msgError);
-		}
+		if (!dom.validate("#xeco-fact", VALIDATORS.factura))
+			return null;
+		const data = i18n.toData();
+		ab.size(lineas) || dom.required("#linea", "Debe detallar los conceptos asociados a la solicitud.");
+		if (((data.subtipo == 3) || (data.subtipo == 9)) && !data.refreb) //subtipo = ttpp o extension
+			dom.addError("#recibo", "Debe indicar un número de recibo válido");
+		if (data.subtipo == 10) //subtipo = deportes
+			dom.required("#extra", VALIDATORS.msgError).leToday("#fMax", VALIDATORS.msgError);
+		if ((data.face == 1)) //factura electronica FACe
+			dom.required("#og", VALIDATORS.msgError).required("#oc", VALIDATORS.msgError).required("#ut", VALIDATORS.msgError);
+		if ((data.face == 2)) //factura electronica Otras
+			dom.required("#plataforma", VALIDATORS.msgError);
 		return data;
 	}
 
